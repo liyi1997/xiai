50a8e6f18d5082ab1492a000061de902024f296f
luhan <luhan@ymtech.xyz>
2019-03-12 15:13:38 +0800

    0.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
+    base: '/xiai/', // 这是部署到github相关的配置 下面会讲
     markdown: {
         lineNumbers: true // 代码块显示行号
     },
@@ -16,16 +16,16 @@ module.exports = {
             { text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
             { text: '博客', link: 'http://obkoro1.com/' }, // 外部链接
             // 下拉列表
-            {
-                text: 'GitHub',
-                items: [
-                    { text: 'GitHub地址', link: 'https://github.com/OBKoro1' },
-                    {
-                        text: '算法仓库',
-                        link: 'https://github.com/OBKoro1/Brush_algorithm'
-                    }
-                ]
-            }
+            // {
+            //     text: 'GitHub',
+            //     items: [
+            //         { text: 'GitHub地址', link: 'https://github.com/OBKoro1' },
+            //         {
+            //             text: '算法仓库',
+            //             link: 'https://github.com/OBKoro1/Brush_algorithm'
+            //         }
+            //     ]
+            // }
         ],
         sidebar: {
             // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
