f2b1363c34307e2322f1cac76efbea92d2a5e06f
luhan <luhan@ymtech.xyz>
2019-03-13 16:42:53 +0800

    22

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push git@github.com:liyi1997/xiai.git
+git push -f git@github.com:liyi1997/xiai.git master:gh-pages
 
 cd -
